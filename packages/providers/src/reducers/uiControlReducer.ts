e9122d7579ca2bb6590ebb94d41bbb4da7ac5576
Tiago Evangelista Pinto <tiagoevanp@hotmail.com>
2025-07-28 15:26:46 -0300

    refactor

--- packages/providers/src/reducers/uiControlReducer.ts
+++ packages/providers/src/reducers/uiControlReducer.ts
@@ -6,6 +6,10 @@ type uiControlDirectionProps = {
     columns: NavigatorProps['columns'];
 };
 
+const hasNoItemSelected = (state: uiControlReducerState) => {
+    return state.column === -1 && state.row === -1;
+};
+
 const getLastMatrixIndexes = (columns: NavigatorProps['columns']) => {
     return { column: columns.length - 1, row: columns[0].length - 1 };
 };
@@ -20,132 +24,112 @@ const getLastColumnIndexOfPreviousRow = ({ row, columns }: Omit<uiControlDirecti
 
 const getNextItem = ({ column, row, columns }: uiControlDirectionProps) => {
     const firstColumnIndexOfNextRow = getFirstColumnIndexOfNextRow({ row, columns });
+    const nextRowIndex = row + 1;
+    const nextColumnIndex = column + 1;
+    const nextRow = columns[firstColumnIndexOfNextRow]?.[nextRowIndex];
 
-    if (columns[column + 1] === undefined) {
-        if (firstColumnIndexOfNextRow === -1) {
-            return getNextItem({ row: 0, column: -1, columns });
-        }
+    if (columns[nextColumnIndex] === undefined) {
+        if (firstColumnIndexOfNextRow === -1) return getNextItem({ row: 0, column: -1, columns });
 
-        if (
-            columns[firstColumnIndexOfNextRow][row + 1]?.props.disabled ||
-            columns[firstColumnIndexOfNextRow][row + 1] === null
-        ) {
-            return getNextItem({ row: row + 1, column: firstColumnIndexOfNextRow, columns });
-        }
+        if (nextRow?.props.disabled || nextRow === null)
+            return getNextItem({ row: nextRowIndex, column: firstColumnIndexOfNextRow, columns });
 
-        return { row: row + 1, column: firstColumnIndexOfNextRow };
+        return { row: nextRowIndex, column: firstColumnIndexOfNextRow };
     }
 
-    if (columns[column + 1][row] === null) {
-        return getNextItem({ row, column: column + 1, columns });
-    }
+    if (columns[nextColumnIndex][row] === null) return getNextItem({ row, column: nextColumnIndex, columns });
 
-    if (columns[column + 1][row]?.props.disabled) {
-        return getNextItem({ row, column: column + 1, columns });
-    }
+    if (columns[nextColumnIndex][row]?.props.disabled) return getNextItem({ row, column: nextColumnIndex, columns });
 
-    return { row, column: column + 1 };
+    return { row, column: nextColumnIndex };
 };
 
 const getPreviousItem = ({ column, row, columns }: uiControlDirectionProps) => {
     const lastColumnIndexOfPreviousRow = getLastColumnIndexOfPreviousRow({ row, columns });
+    const previousRowIndex = row - 1;
+    const previousColumnIndex = column - 1;
 
-    if (columns[column - 1] === undefined) {
+    if (columns[previousColumnIndex] === undefined) {
         const lastMatrixIndexes = getLastMatrixIndexes(columns);
 
-        if (lastColumnIndexOfPreviousRow === -1) {
+        if (lastColumnIndexOfPreviousRow === -1)
             return getPreviousItem({ row: lastMatrixIndexes.row, column: lastMatrixIndexes.column + 1, columns });
-        }
 
         if (
-            columns[lastColumnIndexOfPreviousRow][row - 1]?.props.disabled ||
-            columns[lastColumnIndexOfPreviousRow][row - 1] === null
-        ) {
-            return getPreviousItem({ row: row - 1, column: lastColumnIndexOfPreviousRow, columns });
-        }
+            columns[lastColumnIndexOfPreviousRow][previousRowIndex]?.props.disabled ||
+            columns[lastColumnIndexOfPreviousRow][previousRowIndex] === null
+        )
+            return getPreviousItem({ row: previousRowIndex, column: lastColumnIndexOfPreviousRow, columns });
 
-        return { row: row - 1, column: lastColumnIndexOfPreviousRow };
+        return { row: previousRowIndex, column: lastColumnIndexOfPreviousRow };
     }
 
-    if (columns[column - 1][row] === null) {
-        return getPreviousItem({ row, column: column - 1, columns });
-    }
+    if (columns[previousColumnIndex][row] === null)
+        return getPreviousItem({ row, column: previousColumnIndex, columns });
 
-    if (columns[column - 1][row]?.props.disabled) {
-        return getPreviousItem({ row, column: column - 1, columns });
-    }
+    if (columns[previousColumnIndex][row]?.props.disabled)
+        return getPreviousItem({ row, column: previousColumnIndex, columns });
 
-    return { row, column: column - 1 };
+    return { row, column: previousColumnIndex };
 };
 
 const goUp = ({ column, row, columns }: uiControlDirectionProps) => {
-    if (column === -1 && row === -1) {
-        return { row: 0, column: 0 };
-    }
+    if (hasNoItemSelected({ column, row })) return { row: 0, column: 0 };
 
+    const previousColumnIndex = column - 1;
     const previousRowIndex = row - 1;
     const hasPreviousRow = previousRowIndex >= 0;
     const lastColumnIndex = columns.length - 1;
     const lastRowIndex = columns[lastColumnIndex].length - 1;
 
     if (hasPreviousRow) {
-        if (columns[column][previousRowIndex]?.props.disabled || columns[column][previousRowIndex] === null) {
+        if (columns[column][previousRowIndex]?.props.disabled || columns[column][previousRowIndex] === null)
             return goUp({ column, row: previousRowIndex, columns });
-        }
 
         return { row: previousRowIndex, column };
     }
 
-    if (!hasPreviousRow && columns[column - 1] === undefined) {
-        if (columns[lastColumnIndex][lastRowIndex]?.props.disabled) {
+    if (!hasPreviousRow && columns[previousColumnIndex] === undefined) {
+        if (columns[lastColumnIndex][lastRowIndex]?.props.disabled)
             return goUp({ column: lastColumnIndex, row: lastRowIndex, columns });
-        }
 
         return { row: lastRowIndex, column: lastColumnIndex };
     }
 
-    return goUp({ column: column - 1, row: lastRowIndex + 1, columns });
+    return goUp({ column: previousColumnIndex, row: lastRowIndex + 1, columns });
 };
 
 const goDown = ({ column, row, columns }: uiControlDirectionProps) => {
-    if (column === -1 && row === -1) {
-        return { row: 0, column: 0 };
-    }
+    if (hasNoItemSelected({ column, row })) return { row: 0, column: 0 };
 
     const nextRowIndex = row + 1;
     const hasNextRow = nextRowIndex < columns[column].length;
+    const nextColumnIndex = column + 1;
 
     if (hasNextRow) {
-        if (columns[column][nextRowIndex]?.props.disabled || columns[column][nextRowIndex] === null) {
+        if (columns[column][nextRowIndex]?.props.disabled || columns[column][nextRowIndex] === null)
             return goDown({ column, row: nextRowIndex, columns });
-        }
 
         return { row: nextRowIndex, column };
     }
 
-    if (!hasNextRow && columns[column + 1] === undefined) {
-        if (columns[0][0]?.props.disabled) {
-            return goDown({ column: 0, row: 0, columns });
-        }
+    if (!hasNextRow && columns[nextColumnIndex] === undefined) {
+        if (columns[0][0]?.props.disabled) return goDown({ column: 0, row: 0, columns });
 
         return { row: 0, column: 0 };
     }
 
-    return goDown({ column: column + 1, row: -1, columns });
+    return goDown({ column: nextColumnIndex, row: -1, columns });
 };
 
 const goLeft = ({ column, row, columns }: uiControlDirectionProps) => {
-    if (column === -1 && row === -1) {
-        return { row: 0, column: 0 };
-    }
+    if (hasNoItemSelected({ column, row })) return { row: 0, column: 0 };
 
     return getPreviousItem({ row, column, columns });
 };
 
 const goRight = ({ column, row, columns }: uiControlDirectionProps) => {
-    if (column === -1 && row === -1) {
-        return { row: 0, column: 0 };
-    }
+    if (hasNoItemSelected({ column, row })) return { row: 0, column: 0 };
 
     return getNextItem({ row, column, columns });
 };
@@ -175,9 +159,7 @@ export const uiControlReducer = (state: uiControlReducerState, action: uiControl
         case 'GO_RIGHT':
             return goRight({ ...state, columns: action.columns });
         case 'SELECT':
-            if (state.column !== -1 && state.row !== -1) {
-                action.columns[state.column][state.row]?.props.action();
-            }
+            if (state.column !== -1 && state.row !== -1) action.columns[state.column][state.row]?.props.action();
 
             return state;
         case 'CLEAR':
