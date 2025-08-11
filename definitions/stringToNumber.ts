19e0dd25a5b7092bfdaf5877bc39c37e1c2d1b6f
Tiago Evangelista Pinto <tiagoevanp@hotmail.com>
2025-08-11 15:33:39 -0300

    string to number type definition

--- definitions/stringToNumber.ts
+++ definitions/stringToNumber.ts
@@ -1,2 +1,10 @@
+/**
+ * Converts a string literal type to a number literal type.
+ *
+ * @example
+ * type A = StringToNumber<"42">; // 42
+ * type B = StringToNumber<"hello">; // never
+ */
+
 export type StringToNumber<T extends string> =
-    T extends `${infer Result extends number}` ? Result : never;
+  T extends `${infer Result extends number}` ? Result : never;
